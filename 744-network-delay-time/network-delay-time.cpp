779d7a88bd2b479f15a496cce4af1f35e585d937
Dhairya Kanabar <144530227+Dhairya-Kanabar@users.noreply.github.com>
2026-01-08 14:05:54 +0530

    Time: 90 ms (36.80%) | Memory: 44.3 MB (80.94%) - LeetSync

--- 744-network-delay-time/network-delay-time.cpp
+++ 744-network-delay-time/network-delay-time.cpp
@@ -1,20 +1,46 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& times, int N, int K) {
-        vector<int> dist(N + 1, INT_MAX);
-        dist[K] = 0;
-        for (int i = 0; i < N; i++) {
-            for (vector<int> e : times) {
-                int u = e[0], v = e[1], w = e[2];
-                if (dist[u] != INT_MAX && dist[v] > dist[u] + w) {
-                    dist[v] = dist[u] + w;
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+
+        vector<vector<pair<int,int>>> adj(n+1);
+
+        for(int i=0; i<times.size(); i++){
+            int u =times[i][0];
+            int v = times[i][1];
+            int w= times[i][2];
+            adj[u].push_back({v, w});
+        }
+        
+        vector<int> dist(n+1, INT_MAX);
+        dist[k] = 0;
+
+        priority_queue< pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>> > pq;
+        pq.push({0, k});
+
+        while(!pq.empty() ){
+            int nodeDist = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+            
+            for( auto it : adj[node] ){
+                int neigh = it.first;
+                int weight = it.second;
+
+                if( dist[neigh] > nodeDist + weight ){
+                    dist[neigh] = nodeDist + weight ;
+                    pq.push({dist[neigh], neigh});
                 }
             }
         }
 
-        int maxwait = 0;
-        for (int i = 1; i <= N; i++)
-            maxwait = max(maxwait, dist[i]);
-        return maxwait == INT_MAX ? -1 : maxwait;
+        int ans = 0;
+        for(int i=1; i<n+1; i++){
+            ans = max(ans, dist[i] );
+        }
+
+        if(ans==INT_MAX) return -1;
+
+        return ans;
+
     }
 };
